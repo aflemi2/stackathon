cb88f9ff3bc895d299dcc7b81b11faefa1dc5ce4
Alan Fleming <alalfleming@gmail.com>
2018-05-17 20:26:57 -0400

    body movin

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
-app.use('vendor', express.static(path.join(__dirname, 'node_modules')));
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', (req, res, next)=>res.sendFile(path.join(__dirname, './index.html')));
 
